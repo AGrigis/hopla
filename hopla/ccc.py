d005956862abcba1e1066fd5bd0fa1ebf1818c38
Vincent Frouin <vincent.frouin@cea.fr>
2025-09-11 14:28:36 +0200

    UPD: manage env for pcocc

--- hopla/ccc.py
+++ hopla/ccc.py
@@ -141,7 +141,7 @@ class DelayedCCCJob(DelayedJob):
                     worker_env_insert += [f'export {e}']
             with open(self.paths.worker_file, "r") as fp:
                 tmp = fp.read()
-            worker_env_insert = "\n".join(worker_env_insert) + "\n"
+            worker_env_insert = "\n" + "\n".join(worker_env_insert)
             tmp = tmp.replace('\n$@', f'{worker_env_insert}\n$@')
             with open(self.paths.worker_file, "w") as fp:
                 fp.write(tmp)
